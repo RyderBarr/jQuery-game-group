4ff058fc86127acbb9b7ae96978456b9117bc983
TheProgrammerofalltime <kma69362@gmail.com>
2025-03-11 18:00:07 -0700

    finished

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $("#colorsel1").on('click', function(){
-        $("html").css('')
+        $("body").css({'background-color': 'rgb(240, 194, 194)'})
+    })
+    $("#colorsel2").on('click', function(){
+        $("body").css({'background-color': 'rgb(51, 153, 255)'})
+    })
+    $("#colorsel3").on('click', function(){
+        $("body").css({'background-color': 'rgb(255, 153, 51)'})
     })
 })
